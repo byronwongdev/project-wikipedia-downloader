14f1fd52960fc2113e864b41ad741a86bc30a717
byronwongdev <byronwongdev@gmail.com>
2020-12-17 07:34:47 +0800

    progress update making the gui

---
+++ gui.py
@@ -0,0 +1,18 @@
+import PySimpleGUI as sg      
+
+sg.theme('DarkAmber')    # Keep things interesting for your users
+
+layout = [[sg.Text('Welcome to wikipedia downloader!')],      
+          [sg.Text('Search:', size=(15, 1)), sg.InputText()],      
+          [sg.Submit(), sg.Cancel()],
+          [sg.Output(size=(50,10), key='-Output-')]]      
+
+window = sg.Window('Wikipedia downloader', layout)      
+
+while True:                             # The Event Loop
+    event, values = window.read() 
+    print(event, values)       
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break      
+
+window.close()
